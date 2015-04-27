b0f75f04aeaa719fde3f53f9f84d810ab35ca3d8
Parminder Singh <paramsinghvc@gmail.com>
2015-04-27 22:31:20 +0530

    Feeds Controller

--- app/controllers/activities.js
+++ app/controllers/activities.js
@@ -1,1 +1,24 @@
 var args = arguments[0] || {};
+
+Alloy.Globals.feedsTab = $.feedsTab;
+
+var feeds = Alloy.Collections.instance('feeds');
+
+feeds.url = function() {
+	return Alloy.Globals.apiUri +"feeds?phouse=1";
+};
+feeds.fetch({
+	data : {
+		page : 1
+	}
+});
+
+
+ var feedsRowClick = function(event){
+	 var feedsId = feeds.at(event.index).attributes.id;
+	
+	// var cust = Alloy.createController('feeds/customer', {customerId : customerId}).getView();		
+	// $.customersTab.open(cust);
+		
+	
+};
---
+++ app/models/orders.js
@@ -0,0 +1,29 @@
+exports.definition = {
+	config: {
+	
+		adapter: {
+			type: "restapi",
+			collection_name: "orders",
+			idAttribute: "id"
+		}
+	},
+	extendModel: function(Model) {
+		_.extend(Model.prototype, {
+			initialize : function(args){
+
+			}
+		});
+
+		return Model;
+	},
+	extendCollection: function(Collection) {
+		_.extend(Collection.prototype, {
+			
+			parse: function(resp){
+				
+			}
+		});
+
+		return Collection;
+	}
+};
